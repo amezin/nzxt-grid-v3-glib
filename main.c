64e12ab6c810576e5b9a10f4da3027953b2e37fe
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-03-02 04:41:59 +0600

    Read without libusb

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <gio/gio.h>
-#include <gusb.h>
+#include <gio/gunixinputstream.h>
+
+#include <gudev/gudev.h>
 
 static const guint16 USB_VENDOR_ID_NZXT = 0x1e71;
 static const guint16 USB_PRODUCT_ID_NZXT_GRID_V3 = 0x1711;
-
-static const guint8 NZXT_GRID_ENDPOINT_ADDRESS_IN = 0x81;
-static const guint8 NZXT_GRID_ENDPOINT_ADDRESS_OUT = 0x1;
 
 static const guint8 NZXT_GRID_STATUS_REPORT_ID = 4;
 
@@ -29,85 +33,109 @@ struct nzxt_grid_status_report {
 	guint8 unknown4[5];
 } __attribute__((packed));
 
-static int my_perror(const char *func, GError *err)
-{
-    g_printerr("%s: %s\n", func, (err && err->message) ? err->message : "unknown error");
-    return EXIT_FAILURE;
-}
-
 static void read_callback(GObject *source_object, GAsyncResult *res, gpointer user_data);
 
-static void schedule_read(GUsbDevice *usb_dev)
+static void schedule_read(GInputStream *stream)
 {
     static const gsize BUFFER_SIZE = 64;
     guint8 *data = (guint8 *)g_malloc(BUFFER_SIZE);
 
-    g_usb_device_interrupt_transfer_async(usb_dev, NZXT_GRID_ENDPOINT_ADDRESS_IN, data, BUFFER_SIZE, 0, NULL, read_callback, data);
+    g_input_stream_read_async(stream, data, BUFFER_SIZE, G_PRIORITY_DEFAULT, NULL, read_callback, data);
 }
 
 static gboolean schedule_read_source_cb(gpointer user_data)
 {
-    schedule_read(G_USB_DEVICE(user_data));
+    schedule_read(G_INPUT_STREAM(user_data));
     return FALSE;
 }
 
 static void read_callback(GObject *source_object, GAsyncResult *res, gpointer user_data)
 {
-    GUsbDevice *usb_dev = G_USB_DEVICE(source_object);
+    GInputStream *stream = G_INPUT_STREAM(source_object);
     g_autoptr(GError) err = NULL;
     guint8 *data = (guint8 *)user_data;
-    gssize read_size = g_usb_device_interrupt_transfer_finish(usb_dev, res, &err);
+    gssize read_size = g_input_stream_read_finish(stream, res, &err);
 
     if (read_size < 0) {
-        my_perror("g_usb_device_interrupt_transfer_async", err);
-        g_timeout_add_seconds(1, schedule_read_source_cb, usb_dev);
+        g_warning("g_input_stream_read_async: %s", err->message);
+        g_timeout_add_seconds(1, schedule_read_source_cb, stream);
         return;
     }
 
     if (data[0] != NZXT_GRID_STATUS_REPORT_ID || read_size != sizeof(struct nzxt_grid_status_report)) {
-        fprintf(stderr, "Unexpected report, id = %u, size = %zu\n", data[0], read_size);
-        schedule_read(usb_dev);
+        g_warning("Unexpected report, id = %u, size = %zu\n", data[0], read_size);
+        schedule_read(stream);
         return;
     }
 
     struct nzxt_grid_status_report *status_report = (struct nzxt_grid_status_report *)data;
-    fprintf(stderr, "status: channel %u rpm=%u\n", status_report->channel, GUINT16_FROM_BE(status_report->rpm));
-    schedule_read(usb_dev);
+    g_message("status: channel %u rpm=%u", status_report->channel, GUINT16_FROM_BE(status_report->rpm));
+    schedule_read(stream);
+}
+
+static gchar *find_grid_device()
+{
+    g_autoptr(GUdevClient) client = g_udev_client_new(NULL);
+    g_autolist(GUdevDevice) devices = g_udev_client_query_by_subsystem(client, "hidraw");
+
+    for (GList *l = devices; l != NULL; l = l->next) {
+        GUdevDevice *hidraw_dev = G_UDEV_DEVICE(l->data);
+        g_autoptr(GUdevDevice) hid_dev = g_udev_device_get_parent_with_subsystem(hidraw_dev, "hid", NULL);
+
+        if (!hid_dev) {
+            g_warning("Can't find parent hid device for hidraw device");
+            continue;
+        }
+
+        const gchar *id = g_udev_device_get_property(hid_dev, "HID_ID");
+        unsigned bus_type;
+        unsigned short vendor_id, product_id;
+
+        int n_fields = sscanf(id, "%x:%hx:%hx", &bus_type, &vendor_id, &product_id);
+        if (n_fields != 3) {
+            g_warning("Can't parse %s as HID ID, skipping device", id);
+            continue;
+        }
+
+        g_message("Found device %x:%x", vendor_id, product_id);
+
+        if (vendor_id == USB_VENDOR_ID_NZXT && product_id == USB_PRODUCT_ID_NZXT_GRID_V3) {
+            return g_strdup(g_udev_device_get_device_file(hidraw_dev));
+        }
+    }
+
+    return NULL;
+}
+
+static GInputStream *open_device(const gchar *path)
+{
+    int fd = open(path, O_RDWR);
+
+    if (fd < 0) {
+        perror("open");
+        return NULL;
+    }
+
+    return g_unix_input_stream_new(fd, TRUE);
 }
 
 int main(void)
 {
-    g_autoptr(GError) err = NULL;
-    g_autoptr(GMainLoop) loop = NULL;
-    g_autoptr(GUsbContext) usb_ctx = NULL;
-    g_autoptr(GUsbDevice) usb_dev = NULL;
-    g_autoptr(GUsbInterface) usb_iface = NULL;
+    g_autofree gchar *device_path = find_grid_device();
+    if (!device_path) {
+        g_warning("Can't find NZXT Grid device");
+        return EXIT_FAILURE;
+    }
 
-    loop = g_main_loop_new(NULL, FALSE);
+    g_autoptr(GInputStream) input_stream = open_device(device_path);
+    if (!input_stream) {
+        g_warning("Can't open device");
+        return EXIT_FAILURE;
+    }
 
-    usb_ctx = g_usb_context_new(&err);
-    if (!usb_ctx)
-        return my_perror("g_usb_context_new", err);
-
-    usb_dev = g_usb_context_find_by_vid_pid(usb_ctx, USB_VENDOR_ID_NZXT, USB_PRODUCT_ID_NZXT_GRID_V3, &err);
-    if (!usb_dev)
-        return my_perror("g_usb_context_find_by_vid_pid", err);
-
-    usb_iface = g_usb_device_get_interface(usb_dev, G_USB_DEVICE_CLASS_HID, 0, 0, &err);
-    if (!usb_iface)
-        return my_perror("g_usb_device_get_interface", err);
-
-    if (!g_usb_device_open(usb_dev, &err))
-        return my_perror("g_usb_device_open", err);
-
-    if (!g_usb_device_claim_interface(usb_dev, g_usb_interface_get_number(usb_iface), G_USB_DEVICE_CLAIM_INTERFACE_BIND_KERNEL_DRIVER, &err))
-        return my_perror("g_usb_device_claim_interface", err);
-
-    schedule_read(usb_dev);
+    g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, FALSE);
+    schedule_read(input_stream);
     g_main_loop_run(loop);
-
-    if (!g_usb_device_close(usb_dev, &err))
-        return my_perror("g_usb_device_close", err);
 
     return EXIT_SUCCESS;
 }
