ce8f690dcef62af60adf40b8a8de7665cde8383b
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-03-14 15:16:56 +0600

    WinHidInputStream: get rid of most memory allocations
    There can be only one read operation in progress.

--- winhidinputstream.c
+++ winhidinputstream.c
@@ -4,6 +4,14 @@ struct _GridctlWinHidInputStream {
     WingInputStream parent_instance;
 
     gulong input_report_length;
+
+    void *allocated_buffer;
+
+    void *buffer;
+    gsize count;
+
+    void *orig_buffer;
+    gsize orig_count;
 };
 
 G_DEFINE_TYPE(GridctlWinHidInputStream, gridctl_win_hid_input_stream, WING_TYPE_INPUT_STREAM)
@@ -41,112 +49,82 @@ get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
     }
 }
 
-struct SizedReadContext {
-    void *buffer;
-    gsize count;
-    void *orig_buffer;
-    gsize orig_count;
-};
-
 static void
-sized_read_context_init(struct SizedReadContext *context,
-                        GridctlWinHidInputStream *sized_stream,
-                        void *buffer,
-                        gsize count)
+finalize(GObject *object)
 {
-    context->orig_buffer = buffer;
-    context->orig_count = count;
+    GridctlWinHidInputStream *stream = GRIDCTL_WIN_HID_INPUT_STREAM(object);
 
-    if (count < sized_stream->input_report_length) {
-        context->buffer = g_malloc0(sized_stream->input_report_length);
-        context->count = sized_stream->input_report_length;
-    } else {
-        context->buffer = buffer;
-        context->count = count;
-    }
+    g_clear_pointer(&stream->allocated_buffer, g_free);
+
+    G_OBJECT_CLASS(gridctl_win_hid_input_stream_parent_class)->finalize(object);
 }
 
 static void
-sized_read_context_dispose(struct SizedReadContext *context)
+read_init(GridctlWinHidInputStream *win_hid_stream, void *buffer, gsize count)
 {
-    if (context->buffer && context->buffer != context->orig_buffer) {
-        g_free(context->buffer);
-        context->buffer = NULL;
+    win_hid_stream->orig_buffer = buffer;
+    win_hid_stream->orig_count = count;
+
+    if (count < win_hid_stream->input_report_length) {
+        if (!win_hid_stream->allocated_buffer) {
+            win_hid_stream->allocated_buffer = g_malloc(win_hid_stream->input_report_length);
+        }
+
+        win_hid_stream->buffer = win_hid_stream->allocated_buffer;
+        win_hid_stream->count = win_hid_stream->input_report_length;
+    } else {
+        win_hid_stream->buffer = buffer;
+        win_hid_stream->count = count;
     }
 }
 
 static gssize
-sized_read_context_finish(struct SizedReadContext *context, gssize n_read)
+read_complete(GridctlWinHidInputStream *win_hid_stream, gssize n_read)
 {
-    g_return_val_if_fail(context->buffer != NULL, -1);
-
-    if (n_read <= 0 || context->buffer == context->orig_buffer) {
-        sized_read_context_dispose(context);
+    if (n_read <= 0 || win_hid_stream->buffer == win_hid_stream->orig_buffer) {
         return n_read;
     }
 
     gsize u_n_read = (gsize)n_read;
-    if (u_n_read <= context->orig_count) {
-        memcpy(context->orig_buffer, context->buffer, u_n_read);
-        sized_read_context_dispose(context);
-        return n_read;
+    if (u_n_read > win_hid_stream->orig_count) {
+        u_n_read = win_hid_stream->orig_count;
     }
 
-    memcpy(context->orig_buffer, context->buffer, context->orig_count);
-    sized_read_context_dispose(context);
-    return (gssize)context->orig_count;
-}
-
-static struct SizedReadContext *
-sized_read_context_new(GridctlWinHidInputStream *sized_stream, void *buffer, gsize count)
-{
-    struct SizedReadContext *context = g_new(struct SizedReadContext, 1);
-    sized_read_context_init(context, sized_stream, buffer, count);
-    return context;
-}
-
-static void
-sized_read_context_free(gpointer ptr)
-{
-    if (!ptr) {
-        return;
-    }
-
-    struct SizedReadContext *context = ptr;
-    sized_read_context_dispose(context);
-
-    g_free(ptr);
+    memcpy(win_hid_stream->orig_buffer, win_hid_stream->buffer, u_n_read);
+    return (gssize)u_n_read;
 }
 
 static gssize
 read_fn(GInputStream *stream, void *buffer, gsize count, GCancellable *cancellable, GError **error)
 {
-    GridctlWinHidInputStream *sized_stream = GRIDCTL_WIN_HID_INPUT_STREAM(stream);
+    GridctlWinHidInputStream *win_hid_stream = GRIDCTL_WIN_HID_INPUT_STREAM(stream);
     GInputStreamClass *base_class = G_INPUT_STREAM_CLASS(gridctl_win_hid_input_stream_parent_class);
 
-    struct SizedReadContext context;
-    sized_read_context_init(&context, sized_stream, buffer, count);
+    read_init(win_hid_stream, buffer, count);
 
-    gssize n_read = base_class->read_fn(stream, context.buffer, context.count, cancellable, error);
+    gssize n_read = base_class->read_fn(stream, /* GInputStream *stream */
+                                        win_hid_stream->buffer, /* void *buffer */
+                                        win_hid_stream->count, /* gsize count */
+                                        cancellable, /* GCancellable *cancellable */
+                                        error /* GError **error */);
 
-    return sized_read_context_finish(&context, n_read);
+    return read_complete(win_hid_stream, n_read);
 }
 
 static void
 read_async_callback(GObject *source_object, GAsyncResult *result, gpointer user_data)
 {
-    GInputStream *stream = G_INPUT_STREAM(source_object);
+    GridctlWinHidInputStream *win_hid_stream = GRIDCTL_WIN_HID_INPUT_STREAM(source_object);
+    GInputStream *stream = G_INPUT_STREAM(win_hid_stream);
     GInputStreamClass *base_class = G_INPUT_STREAM_CLASS(gridctl_win_hid_input_stream_parent_class);
 
     g_autoptr(GTask) task = user_data;
-    struct SizedReadContext *context = g_task_get_task_data(task);
-
     GError *error = NULL;
 
     gssize n_read = base_class->read_finish(stream, result, &error);
-    n_read = sized_read_context_finish(context, n_read);
+    n_read = read_complete(win_hid_stream, n_read);
 
-    if (n_read < 0) {
+    if (error) {
         g_task_return_error(task, error);
     } else {
         g_task_return_int(task, n_read);
@@ -162,18 +140,17 @@ read_async(GInputStream *stream,
            GAsyncReadyCallback callback,
            gpointer user_data)
 {
-    GridctlWinHidInputStream *sized_stream = GRIDCTL_WIN_HID_INPUT_STREAM(stream);
+    GridctlWinHidInputStream *win_hid_stream = GRIDCTL_WIN_HID_INPUT_STREAM(stream);
     GInputStreamClass *base_class = G_INPUT_STREAM_CLASS(gridctl_win_hid_input_stream_parent_class);
 
     GTask *task = g_task_new(stream, cancellable, callback, user_data);
     g_task_set_source_tag(task, read_async);
 
-    struct SizedReadContext *context = sized_read_context_new(sized_stream, buffer, count);
-    g_task_set_task_data(task, context, sized_read_context_free);
+    read_init(win_hid_stream, buffer, count);
 
     base_class->read_async(stream, /* GInputStream *stream */
-                           context->buffer, /* void *buffer */
-                           context->count, /* gsize count */
+                           win_hid_stream->buffer, /* void *buffer */
+                           win_hid_stream->count, /* gsize count */
                            io_priority, /* int io_priority */
                            cancellable, /* GCancellable *cancellable */
                            read_async_callback, /* GAsyncReadyCallback callback */
@@ -193,6 +170,7 @@ gridctl_win_hid_input_stream_class_init(GridctlWinHidInputStreamClass *class)
     GObjectClass *gobject_class = G_OBJECT_CLASS(class);
     gobject_class->get_property = get_property;
     gobject_class->set_property = set_property;
+    gobject_class->finalize = finalize;
 
     GInputStreamClass *input_stream_class = G_INPUT_STREAM_CLASS(class);
     input_stream_class->read_fn = read_fn;
@@ -206,7 +184,8 @@ gridctl_win_hid_input_stream_class_init(GridctlWinHidInputStreamClass *class)
         0, /* gulong minimum */
         G_MAXULONG, /* gulong maximum */
         0, /* gulong default_value */
-        G_PARAM_READWRITE | G_PARAM_CONSTRUCT | G_PARAM_STATIC_STRINGS /* GParamFlags flags */);
+        G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY
+            | G_PARAM_STATIC_STRINGS /* GParamFlags flags */);
 
     g_object_class_install_properties(gobject_class, PROP_COUNT, props);
 }
